9bfff9854821970ac85f96041db36d0ce33ebaee
karimabdelhamid <karimsherif305@gmail.com>
2024-11-02 16:09:54 -0400

    Fixed routing for GPview

--- src/App.js
+++ src/App.js
@@ -9,7 +9,11 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/FamilyMedicineView" component={FamilyMedicineView} />
+        <Route path="*">
+          <div style={{ padding: "20px", textAlign: "center" }}>404 - Page Not Found</div>
+        </Route>
       </Switch>
+
     </Router>
   );
 }
