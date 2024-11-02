5b57fb03a70744be2556ae2adaa65c5de0be1b80
karimabdelhamid <karimsherif305@gmail.com>
2024-11-02 16:07:54 -0400

    Fixed routing for GPview

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import GPview from './pages/GPview';
+import FamilyMedicineView from './pages/FamilyMedicineView';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/GPview" component={GPview} />
+        <Route path="/FamilyMedicineView" component={FamilyMedicineView} />
       </Switch>
     </Router>
   );
